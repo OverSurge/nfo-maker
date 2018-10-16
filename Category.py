91cd19e3f01a5a7a2c5b6adb84f64112c04acf9b
OverSurge <11167007+OverSurge@users.noreply.github.com>
2018-10-16 12:30:23 +0200

    Better Category print + sample Line names
    Category name prints filler on sides
    Added French sample Line names

--- Category.py
+++ Category.py
@@ -2,8 +2,10 @@ from Line import Line
 
 
 class Category:
+    filler = "="
+
     def __init__(self, name: str, lines: list=None):
-        self.name = name
+        self.name = " " + name + " "
         if lines is None:
             self.lines = []
         else:
@@ -13,9 +15,12 @@ class Category:
         return max(len(self.name)+2, max([len(x) for x in self.lines]))
 
     def __repr__(self):
-        res = ""
+        print(len(self), len(self.name))
+        fill = (len(self) - len(self.name)) // 2 * self.filler
+        res = fill + self.name + fill + "\n"
         for line in self.lines:
-            res += line
+
+            res = res + str(line) + "\n"
         return res
 
     def add_line(self, line: Line):
--- tests/test_category.py
+++ tests/test_category.py
@@ -5,11 +5,12 @@ from Line import Line
 
 class CategoryPrint(unittest.TestCase):
     def test(self):
-        cat = Category("cat")
+        cat = Category("Infos Générales")
         l1 = Line("Date", "01/01/18")
         l2 = Line("OS", "Linux")
         self.assertEqual(len(l1), 15)
         self.assertEqual(len(l2), 10)
         cat.add_line(l1)
         cat.add_line(l2)
-        self.assertEqual(len(cat), 15)
+        self.assertEqual(len(cat), 19)
+        print(cat)
